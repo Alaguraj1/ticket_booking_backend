4abb250d10ac07df7c434f1eb015495bb1f757a2
Alaguraj1 <alaguraj259@gmail.com>
2024-12-11 18:04:47 +0530

    changes

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,17 +1,28 @@
 const Login = require("../Models/authSchema");
 
-
 exports.login = async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const user = await Login.findOne({ email: email, password: password });
-        if (user) {
-            res.send(user);
-        } else {
-            res.status(401).json("Invalid Login");
-        }
-    } catch (err) {
-        res.status(500).json(err);
-        console.log(err.message);
-    }
-}
+  try {
+    res.status(200).json({
+      success: true,
+      message: "Login Successfully",
+    });
+  } catch(err) {
+    res.status(404).json({
+      success: false,
+      message: err.message,
+    });
+  }
+
+  // try {
+  //     const { email, password } = req.body;
+  //     const user = await Login.findOne({ email: email, password: password });
+  //     if (user) {
+  //         res.send(user);
+  //     } else {
+  //         res.status(401).json("Invalid Login");
+  //     }
+  // } catch (err) {
+  //     res.status(500).json(err);
+  //     console.log(err.message);
+  // }
+};
